27e409f2cc956eabf103d1850576f750efd31053
pj perry <pjperry28@gmail.com>
2022-07-19 21:04:56 -0700

    write first test and got a good fail

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -5,5 +5,20 @@ using PierresBakery.Models;
 
 namespace BreadTest.Tests
 {
- 
+   [TestClass]
+    public class BreadTests
+    {
+        [TestMethod]
+        public void  BreadCost_CostOneLoafNoDiscount_Int()
+        
+        {
+            //Arrange
+            int BreadQuantity = 1;
+            double BreadCost = 5;
+            //Act
+           Bread BreadOrder = new Bread();
+            //Assert
+            Assert.AreEqual(BreadCost, BreadOrder.BreadCost());
+        }
+   }
 }
